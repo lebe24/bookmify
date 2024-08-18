530bd0f6069df282f6081c27698f76a3d7d22fcb
Emmanuel Philip <31292486+lebe24@users.noreply.github.com>
2024-08-18 21:05:20 +0100

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://fdkudaenyarfoteevtqr.supabase.co"
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZka3VkYWVueWFyZm90ZWV2dHFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM3NzAyNzIsImV4cCI6MjAwOTM0NjI3Mn0.iYc5YLr8XU3s2vifzYPDD1gVITJFTtAgxjFmIfg4FdU"
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL 
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
