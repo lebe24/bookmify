ab5918318b2c72b83c2265f53b1e76d85d48d421
Emmanuel Philip <31292486+lebe24@users.noreply.github.com>
2024-08-24 11:22:16 +0000

    update fix: name

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -10,7 +10,7 @@ const Badge = () => {
     <span className="relative inline-block w-fit mx-auto overflow-hidden rounded-full p-[1px]">
       <span className="absolute inset-[-1000%] animate-[spin_3s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#71717a_0%,#27272a_50%,#71717a_100%)]" />
       <div className="flex gap-1 h-full w-full items-center justify-center rounded-full bg-zinc-950 px-3 py-1 text-sm text-zinc-200 backdrop-blur-3xl">
-        <p>From now on, please use <a href="https://bookmify.vercel.app/" className="font-bold underline">Bookmify</a>! &#127881;</p>
+        <p>From now on, please use <a href="https://Bookmark-Ai.vercel.app/" className="font-bold underline">Bookmark-Ai</a>! &#127881;</p>
         <Cancel className="cursor-pointer text-zinc-500 hover:text-zinc-200 duration-200" width={16} onClick={() => setIsOpen(false)}/>
       </div>
     </span>
